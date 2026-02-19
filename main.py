7f6040cd1d85b2cefd3ff48cfd808efd3cdc58c0
Hisham Shariq Abdullah <65910788+hishamabd235@users.noreply.github.com>
2026-02-19 23:22:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 # AIM: WAP to find students appearing for entrance exams
-# Coder:
-# Date:
+# Coder: Hisham Shariq Abdullah
+# Date: 19/02/26
 
 print("--- Entrance Exam Students ---")
+
 cet_students = {"Alice", "Bob", "Charlie", "David"}
 jee_students = {"Eve", "Frank", "Grace", "Heidi"}
 neet_students = {"Ivan", "Judy", "Karl", "Liam"}
 
-print(f"List of Students:")
-print(f"CET Students: {cet_students}")
-print(f"JEE Students: {jee_students}")
-print(f"NEET Students: {neet_students}")
+print("\nList of Students:")
+print("CET Students:", cet_students)
+print("JEE Students:", jee_students)
+print("NEET Students:", neet_students)
 
-# Write your code here
 # TODO: Find and Print All the Students appearing for any Entrance Exam
+all_students = cet_students.union(jee_students, neet_students)
+print("All Students:", all_students)
 
 # TODO: Find and Print Students appearing for All Entrance Exams
+all_exams = cet_students.intersection(jee_students, neet_students)
+print("All Exams:", all_exams)
 
 # TODO: Find and Print Students appearing for JEE but not for NEET
+jee_not_neet = jee_students.difference(neet_students)
+print("JEE but not for NEET:", jee_not_neet)
 
 # TODO: Find and Print Students appearing for CET and NEET but not for JEE
+cet_neet_not_jee = cet_students.intersection(neet_students).difference(jee_students)
+print("CET and NEET but not for JEE:", cet_neet_not_jee)
